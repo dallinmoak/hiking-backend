c3f76d8ccf929f5ea45b9fd8975dfe6d5a0e24c8
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2026-02-24 15:05:18 -0700

    track the neon_auth.users table, but tell drizzle not to write to any schema but public.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
+  schemaFilter: ['public'],
   dbCredentials: {
     url: process.env.DB_URL!,
   }
